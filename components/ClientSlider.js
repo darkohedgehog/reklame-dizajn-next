232392c5c3617935835411c0d4bfb5fec3ff8128
darkohedgehog <zivic.darko79@gmail.com>
2023-10-14 18:37:44 +0200

    New file

---
+++ components/ClientSlider.js
@@ -0,0 +1,30 @@
+// clients data
+const clientsData = [
+    {
+      image: '/t-avt-1.png',
+      name: 'Anne Smith',
+      position: 'Customer',
+      message:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+    },
+    {
+      image: '/t-avt-2.png',
+      name: 'Jane Doe',
+      position: 'Customer',
+      message:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+    },
+    {
+      image: '/t-avt-3.png',
+      name: 'Jhon Doe',
+      position: 'Customer',
+      message:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+    },
+  ];
+  
+  const ClientSlider = () => {
+    return <div>Testimonial Slider</div>;
+  };
+  
+  export default ClientSlider;
