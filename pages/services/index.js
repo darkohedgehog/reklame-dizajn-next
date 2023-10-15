3642a9b448e6053d554dd62d7f0c4b90108f964c
darkohedgehog <zivic.darko79@gmail.com>
2023-10-15 18:19:42 +0200

    Modified files

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,3 +1,15 @@
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+
+
+export const serviceData = [
+
+];
+
+
+
 const Services = () => {
     return <div>Services</div>;
   };
