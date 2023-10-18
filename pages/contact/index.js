5d053b5ac844c3916cd33717a05b0b3569dd1830
darkohedgehog <zivic.darko79@gmail.com>
2023-10-18 18:44:21 +0200

    Modified file

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,25 @@
+import Circles from "/components/Circles";
+import {BsArrowRight} from "react-icons/bs";
+import { motion } from "framer-motion";
+import {fadeIn} from "../../variants";
+
+
+
+
+
 const Contact = () => {
-    return <div>Contact</div>;
+    return (
+      <div className="h-full bg-primary/30">
+        <div className="container mx-auto py-32 text-center">
+          <div className="">
+            {/* text */}
+            <h2>
+              Let`s <span>connect.</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
   };
   
   export default Contact;
