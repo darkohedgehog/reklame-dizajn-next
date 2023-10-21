3619a36e8fead30c0bf03cc00d82a6f91649334e
darkohedgehog <zivic.darko79@gmail.com>
2023-10-21 19:56:18 +0200

    Modified file

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import {fadeIn} from "../variants";
 
 const Home = () => {
     return (
-      <div className="bg-primary/60 h-full flex flex-col">
+      <div className="bg-primary/60 h-full">
         {/* text */}
         <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/10">
           <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
